3e3950c06e38db90122be3b2a9cb1451a0347b97
paquito05 <samuel.john.11.jsas@gmail.com>
2021-10-20 12:21:48 -0700

    Initial Commit

---
+++ SoundBoard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundBoard
+//
+//  Created by John Samuel Altamirano Sanchez on 10/20/21.
+//  Copyright © 2021 empresa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
